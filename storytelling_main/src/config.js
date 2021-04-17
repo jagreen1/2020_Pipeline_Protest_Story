74433cc06ab09701ff09392c4d60f6cdeaecb515
jagreen1 <66330451+jagreen1@users.noreply.github.com>
2021-04-17 14:32:34 -0400

    Update config.js

--- storytelling_main/src/config.js
+++ storytelling_main/src/config.js
@@ -39,7 +39,7 @@ const config = {
     description: '<p>Dec 31 - In response to ongoing obstruction of the Coastal GasLink Project by members of the Wet’suwet’en First Nation, the B.C. Supreme Court issue an injunction ordering protestors to stop. It calls for the removal of any obstructions including cabins and gates on any roads, bridges or work sites the company has been authorized to use. Shortly after the injunction is issued the RCMP arrest 14 protesters.</p><p>Jan 1 - The following day the Wet’suwet’en First Nation herediatary chiefs serve Coastal GasLink with an eviction notice, rejecting the injunction. They order the RCMP and Coastal GasLink personnel from the Wetʼsuwetʼen territory, stating that company workers are trespassing on their unceded territory.</p>',
     location: {
       center: [-127.36335, 54.19083],
-      zoom: 8,
+      zoom: 10,
       pitch: 43.50,
       bearing: 0.00,
     },
